511b8fc747c05fc133943ece78256f183c99396b
ut0729 <fasolato@omicronconsulting.it>
2018-02-23 12:38:44 +0100

    CRUD functions

--- JFMigrate/src/main/java/net/fasolato/jfmigrate/internal/MysqlDialectHelper.java
+++ JFMigrate/src/main/java/net/fasolato/jfmigrate/internal/MysqlDialectHelper.java
@@ -6,6 +6,7 @@ import net.fasolato.jfmigrate.builders.*;
 import java.sql.JDBCType;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class MysqlDialectHelper implements IDialectHelper {
 
@@ -316,14 +317,98 @@ public class MysqlDialectHelper implements IDialectHelper {
     }
 
     public List<Pair<String, Object[]>> getInsertCommand(Data d) {
-        return null;
+        List<Pair<String, Object[]>> toReturn = new ArrayList<Pair<String, Object[]>>();
+
+        for (Map<String, Object> m : d.getData()) {
+            String sql = "";
+            List<Object> values = new ArrayList<Object>();
+
+            sql += " INSERT INTO " + d.getTableName() + " (";
+            int i = 0;
+            for (String k : m.keySet()) {
+                sql += k;
+                if (i < m.keySet().size() - 1) {
+                    sql += ", ";
+                }
+                i++;
+            }
+            sql += " ) VALUES (";
+            i = 0;
+            for (String k : m.keySet()) {
+                sql += "?";
+                values.add(m.get(k));
+                if (i < m.keySet().size() - 1) {
+                    sql += ", ";
+                }
+                i++;
+            }
+            sql += " ); ";
+
+            toReturn.add(new Pair<String, Object[]>(sql, values.toArray()));
+        }
+
+        return toReturn;
     }
 
     public List<Pair<String, Object[]>> getDeleteCommand(Data d) {
-        return null;
+        List<Pair<String, Object[]>> toReturn = new ArrayList<Pair<String, Object[]>>();
+
+        if (!d.isAllRows()) {
+            for (Map<String, Object> w : d.getWhere()) {
+                String sql = "";
+                List<Object> values = new ArrayList<Object>();
+
+                sql += " DELETE FROM " + d.getTableName() + " WHERE 1 = 1 ";
+                for (String k : w.keySet()) {
+                    sql += " AND " + k + " = ? ";
+                    values.add(w.get(k));
+                }
+                sql += ";";
+
+                toReturn.add(new Pair<String, Object[]>(sql, values.toArray()));
+            }
+        } else {
+            String sql = " DELETE FROM " + d.getTableName() + "; ";
+            toReturn.add(new Pair<String, Object[]>(sql, new Object[0]));
+        }
+
+        return toReturn;
     }
 
     public List<Pair<String, Object[]>> getUpdateCommand(Data d) {
-        return null;
+        List<Pair<String, Object[]>> toReturn = new ArrayList<Pair<String, Object[]>>();
+
+        for (int i = 0; i < d.getData().size(); i++) {
+            Map<String, Object> m = d.getData().get(i);
+
+            String sql = "";
+            List<Object> values = new ArrayList<Object>();
+
+            sql += " UPDATE " + d.getTableName() + " SET ";
+            int j = 0;
+            for (String k : m.keySet()) {
+                sql += k + " = ? ";
+                values.add(m.get(k));
+                if (j < m.keySet().size() - 1) {
+                    sql += ", ";
+                }
+                j++;
+            }
+            if (!d.isAllRows()) {
+                sql += " WHERE 1 = 1 ";
+                for (Map<String, Object> w : d.getWhere()) {
+                    for (String k : w.keySet()) {
+                        sql += " AND " + k + " = ? ";
+                        values.add(w.get(k));
+                    }
+                }
+            }
+
+            sql += ";";
+
+            toReturn.add(new Pair<String, Object[]>(sql, values.toArray()));
+        }
+
+        return toReturn;
     }
 }
